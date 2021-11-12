81fb045d035f8446fba751c4a57aa0642f7b0791
Jefferson <silva.o.jefferson@gmail.com>
2021-11-12 09:42:39 -0300

    Update credentials

--- db/credentials.py
+++ db/credentials.py
@@ -1,4 +1,17 @@
-from utils.filehelper import get_property
+def get_property(property):
+    """
+    Return a property in the .db.properties file
+    Args:
+        property: name of the property. E.g. db_user
+    """
+    with open(".db.properties") as f:
+        line = f.readline()
+        while (line):
+            if (property in line):
+                p = line.split(":")[1]
+                return p.strip()
+            line = f.readline()
+    return None
 
 DB_USER = get_property("db_user")
 DB_PWD = get_property("db_pwd")
