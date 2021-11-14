e02c06784eaf03994486072a86d18c2a2bd30ab4
Jefferson <silva.o.jefferson@gmail.com>
2021-11-13 21:08:30 -0300

    Shift the entities_map to a separate file

--- constants.py
+++ constants.py
@@ -1,27 +1,5 @@
-from db.tables.tb_definitions import *
-
 SUCCESSFUL = 200
 UNAUTHORIZED = 401
-
-"""
-entities_map = {
-    "source": TableSources,
-    "media": TableMedia,
-    "movement": TableMovements,
-    "people": TablePeople,
-    "educational": TableEducationalInstitutions,
-    "private": TablePrivateInstitutions,
-    "public": TablePublicInstitutions,
-    "actions": TableRacistActions,
-    "city": TableCities,
-    "state": TableStates,
-    "country": TableCountries,
-    "work": TableWorks,
-    "police": TablePolices,
-    "law": TableLaws,
-    "text": TableArticles,
-}
-"""
 
 API_KEY_SIZE = 32
 PERM_LEVEL = {
---
+++ db/entities.py
@@ -0,0 +1,19 @@
+from db.tables.tb_definitions import *
+
+entities_map = {
+    "source": TableSources,
+    "media": TableMedia,
+    "movement": TableMovements,
+    "people": TablePeople,
+    "educational": TableEducationalInstitutions,
+    "private": TablePrivateInstitutions,
+    "public": TablePublicInstitutions,
+    "actions": TableRacistActions,
+    "city": TableCities,
+    "state": TableStates,
+    "country": TableCountries,
+    "work": TableWorks,
+    "police": TablePolices,
+    "law": TableLaws,
+    "text": TableArticles,
+}
