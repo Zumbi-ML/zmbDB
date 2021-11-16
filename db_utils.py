54a5023e36ac10dc45ecd2b566e38874d6d98143
Jefferson <silva.o.jefferson@gmail.com>
2021-11-16 20:05:56 -0300

    Change db_service to db_utils

--- db_utils.py
+++ db_utils.py
@@ -4,7 +4,7 @@ from sqlalchemy import MetaData, Table, Column
 from sqlalchemy import Integer, String, Date, ForeignKey, UniqueConstraint
 from sqlalchemy_utils import database_exists, create_database, drop_database
 
-class DatabaseService(object):
+class DatabaseUtils(object):
 
     def drop_n_create_db():
         """
