288d674194370e74b5d8969dbb395a2f2457f639
Jefferson <silva.o.jefferson@gmail.com>
2021-11-25 19:36:03 -0300

    Fix get_engine function signature

--- db/credentials.py
+++ db/credentials.py
@@ -8,7 +8,7 @@ DB_HOST = get_property("db_host")
 DB_NAME = get_property("db_name")
 DB_DEBUG_MODE = get_property("db_debug_mode") == "True"
 
-def get_engine(db_user=DB_USER, db_pwd=DB_PWD, db_host=DB_HOST, db_name=DB_NAME):
+def get_engine(db_user=DB_USER, db_pwd=DB_PWD, db_host=DB_HOST, db_name=DB_NAME, db_debug_mode=DB_DEBUG_MODE):
     return create_engine(f"mysql://{db_user}:{db_pwd}@{db_host}/{db_name}", echo=DB_DEBUG_MODE)
 
 def get_session():
