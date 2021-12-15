37513a0e24c01e3dda7095c5584795ab12dc7af9
Jefferson <silva.o.jefferson@gmail.com>
2021-12-15 15:36:57 -0300

    Add restart db script

--- db_utils.py
+++ db_utils.py
@@ -4,14 +4,12 @@ from sqlalchemy import MetaData, Table, Column
 from sqlalchemy import Integer, String, Date, ForeignKey, UniqueConstraint
 from sqlalchemy_utils import database_exists, create_database, drop_database
 
-class DatabaseUtils(object):
-
-    def drop_n_create_db():
-        """
-        Drop DB (if existent) and create db schema
-        """
-        engine = get_engine()
-        if database_exists(engine.url):
-            drop_database(engine.url)
-        create_database(engine.url)
-        Base.metadata.create_all(engine)
+def drop_n_create_db():
+    """
+    Drop DB (if existent) and create db schema
+    """
+    engine = get_engine()
+    if database_exists(engine.url):
+        drop_database(engine.url)
+    create_database(engine.url)
+    Base.metadata.create_all(engine)
---
+++ restart-db.py
@@ -0,0 +1,3 @@
+from db_utils import drop_n_create_db
+
+drop_n_create_db()
