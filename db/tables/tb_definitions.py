43556dc82a1217a571d24d852f1921bfa24561f3
Jefferson <silva.o.jefferson@gmail.com>
2021-12-16 11:31:29 -0300

    Increase entity name field's size

--- db/tables/tb_definitions.py
+++ db/tables/tb_definitions.py
@@ -29,7 +29,7 @@ class TableArticles(Base):
     keywords = Column('keywords', String(MAX_KEYWORDS))
     section = Column('section', String(MAX_SECTION))
     site_name = Column('site_name', String(MAX_SITE_NAME))
-    source = Column('source', String(20))
+    source = Column('source', String(MAX_ENTITY_NAME_FIELD))
     authors = Column('authors', String(MAX_AUTHORS))
     added = Column('added', Date)
     last_modified = Column('last_modified', Date)
@@ -39,96 +39,96 @@ class TableSources(Base):
     __tablename__ = "tb_sources"
     id = Column('id', Integer, primary_key=True)
     hashed_url = Column('hashed_url', BigInteger, nullable=False)
-    name = Column('name', String(50), nullable=False)
+    name = Column('name', String(MAX_ENTITY_NAME_FIELD), nullable=False)
 
 class TableMedia(Base):
     __tablename__ = "tb_media"
     id = Column('id', Integer, primary_key=True)
     hashed_url = Column('hashed_url', BigInteger, ForeignKey('tb_articles.hashed_url'), nullable=False)
-    name = Column('name', String(50), nullable=False)
+    name = Column('name', String(MAX_ENTITY_NAME_FIELD), nullable=False)
     table_articles = relationship("TableArticles")
 
 class TableMovements(Base):
     __tablename__ = "tb_movements"
     id = Column('id', Integer, primary_key=True)
     hashed_url = Column('hashed_url', BigInteger, ForeignKey('tb_articles.hashed_url'), nullable=False)
-    name = Column('name', String(50), nullable=False)
+    name = Column('name', String(MAX_ENTITY_NAME_FIELD), nullable=False)
     table_articles = relationship("TableArticles")
 
 class TablePeople(Base):
     __tablename__ = "tb_people"
     id = Column('id', Integer, primary_key=True)
     hashed_url = Column('hashed_url', BigInteger, ForeignKey('tb_articles.hashed_url'), nullable=False)
-    name = Column('name', String(50), nullable=False)
+    name = Column('name', String(MAX_ENTITY_NAME_FIELD), nullable=False)
     table_articles = relationship("TableArticles")
 
 class TableEducationalInstitutions(Base):
     __tablename__ = "tb_educ_inst"
     id = Column('id', Integer, primary_key=True)
     hashed_url = Column('hashed_url', BigInteger, ForeignKey('tb_articles.hashed_url'), nullable=False)
-    name = Column('title', String(50), nullable=False)
+    name = Column('title', String(MAX_ENTITY_NAME_FIELD), nullable=False)
     table_articles = relationship("TableArticles")
 
 class TablePrivateInstitutions(Base):
     __tablename__ = "tb_private_inst"
     id = Column('id', Integer, primary_key=True)
     hashed_url = Column('hashed_url', BigInteger, ForeignKey('tb_articles.hashed_url'), nullable=False)
-    name = Column('title', String(50), nullable=False)
+    name = Column('title', String(MAX_ENTITY_NAME_FIELD), nullable=False)
     table_articles = relationship("TableArticles")
 
 class TablePublicInstitutions(Base):
     __tablename__ = "tb_public_inst"
     id = Column('id', Integer, primary_key=True)
     hashed_url = Column('hashed_url', BigInteger, ForeignKey('tb_articles.hashed_url'), nullable=False)
-    name = Column('title', String(50), nullable=False)
+    name = Column('title', String(MAX_ENTITY_NAME_FIELD), nullable=False)
     table_articles = relationship("TableArticles")
 
 class TableRacistActions(Base):
     __tablename__ = "tb_racist_actions"
     id = Column('id', Integer, primary_key=True)
     hashed_url = Column('hashed_url', BigInteger, ForeignKey('tb_articles.hashed_url'), nullable=False)
-    name = Column('name', String(50), nullable=False)
+    name = Column('name', String(MAX_ENTITY_NAME_FIELD), nullable=False)
     table_articles = relationship("TableArticles")
 
 class TableWorks(Base):
     __tablename__ = "tb_works"
     id = Column('id', Integer, primary_key=True)
     hashed_url = Column('hashed_url', BigInteger, ForeignKey('tb_articles.hashed_url'), nullable=False)
-    name = Column('name', String(50), nullable=False)
+    name = Column('name', String(MAX_ENTITY_NAME_FIELD), nullable=False)
     table_articles = relationship("TableArticles")
 
 class TableCities(Base):
     __tablename__ = "tb_cities"
     id = Column('id', Integer, primary_key=True)
     hashed_url = Column('hashed_url', BigInteger, ForeignKey('tb_articles.hashed_url'), nullable=False)
-    name = Column('name', String(50), nullable=False)
+    name = Column('name', String(MAX_ENTITY_NAME_FIELD), nullable=False)
     table_articles = relationship("TableArticles")
 
 class TableStates(Base):
     __tablename__ = "tb_states"
     id = Column('id', Integer, primary_key=True)
     hashed_url = Column('hashed_url', BigInteger, ForeignKey('tb_articles.hashed_url'), nullable=False)
-    name = Column('name', String(50), nullable=False)
+    name = Column('name', String(MAX_ENTITY_NAME_FIELD), nullable=False)
     table_articles = relationship("TableArticles")
 
 class TableCountries(Base):
     __tablename__ = "tb_countries"
     id = Column('id', Integer, primary_key=True)
     hashed_url = Column('hashed_url', BigInteger, ForeignKey('tb_articles.hashed_url'), nullable=False)
-    name = Column('name', String(50), nullable=False)
+    name = Column('name', String(MAX_ENTITY_NAME_FIELD), nullable=False)
     table_articles = relationship("TableArticles")
 
 class TableLaws(Base):
     __tablename__ = "tb_laws"
     id = Column('id', Integer, primary_key=True)
     hashed_url = Column('hashed_url', BigInteger, ForeignKey('tb_articles.hashed_url'), nullable=False)
-    title = Column('title', String(50))
-    code = Column('code', String(50))
+    title = Column('title', String(MAX_ENTITY_NAME_FIELD))
+    code = Column('code', String(MAX_ENTITY_NAME_FIELD))
     table_articles = relationship("TableArticles")
 
 class TablePolices(Base):
     __tablename__ = "tb_polices"
     id = Column('id', Integer, primary_key=True)
     hashed_url = Column('hashed_url', BigInteger, ForeignKey('tb_articles.hashed_url'), nullable=False)
-    name = Column('name', String(50), nullable=False)
+    name = Column('name', String(MAX_ENTITY_NAME_FIELD), nullable=False)
     table_articles = relationship("TableArticles")
