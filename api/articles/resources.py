1dfa0138963e7663f4cf2e0a0fb6239e5fb281ed
Jefferson <silva.o.jefferson@gmail.com>
2022-01-24 15:23:27 -0300

    Split the parser into two: search_parser and add_parser

--- api/articles/resources.py
+++ api/articles/resources.py
@@ -1,25 +1,27 @@
 # encoding: utf-8
 import article_manager
-from db.article_service import ArticleService
 from flask_restx import Resource, Namespace, reqparse
 import json
-from labels import *
 from zmbapi_exceptions import ZmbDuplicateKeyException
 from zmb_codes import StatusCode
 from zmb_labels import ZmbLabels
 from responder import json_response
 from hasher import hash_url
 
-parser = reqparse.RequestParser()
+search_parser = reqparse.RequestParser()
+for label in ZmbLabels.Article.Entity.all_labels_n_metainfo():
+    search_parser.add_argument(label, type=str)
+
+add_parser = reqparse.RequestParser()
 for label in ZmbLabels.Article.all_labels():
-    parser.add_argument(label, type=str)
+    add_parser.add_argument(label, type=str)
 
 article = Namespace('Article', description='Article resources')
 
 @article.route('/')
 class ArticleResource(Resource):
 
-    @article.expect(parser)
+    @article.expect(search_parser)
     @article.response(200, "Success")
     def get(self):
         """
@@ -28,11 +30,10 @@ class ArticleResource(Resource):
         Example of incoming JSON data
         {"sources":["src1"], "media":["med1","med2","med3"]}}
         """
-        entity_criteria = parser.parse_args()
-        with ArticleService() as article_svc:
-             return article_svc.get_articles_by_criteria(entity_criteria)
+        entity_criteria = search_parser.parse_args()
+        return article_manager.get_articles_by_criteria(entity_criteria)
 
-    @article.expect(parser)
+    @article.expect(add_parser)
     def post(self):
         """
         Adds an article to the database.
@@ -53,17 +54,18 @@ class ArticleResource(Resource):
             "meta_data": "article's metadata",
         }
         """
-        parsed_args = parser.parse_args()
+        parsed_args = add_parser.parse_args()
 
         article_map = article_manager.setup_types(parsed_args)
-        hashed_url = article_map['hashed_url']
+        hashed_url = article_map[ZmbLabels.Article.HashedURL.api()]
         try:
             article_manager.add_article_n_entities(article_map)
         except ZmbDuplicateKeyException as e:
             msg = f"{hashed_url}\t{str(e)}"
             status_code = StatusCode.DUPLICATE_KEY.code()
             return json_response(status_code=status_code, message=msg)
-        return json_response(status_code=StatusCode.SUCCESS.code(), message=article_map['url'])
+        URL_lbl = ZmbLabels.Article.URL.api()
+        return json_response(status_code=StatusCode.SUCCESS.code(), message=article_map[URL_lbl])
 
 @article.route('/<hashed_url>')
 class ArticleIdResource(Resource):
